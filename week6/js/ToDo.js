fa8218b077f055a1a91b3d0b47594986c82c0d0c
Harper-Christopher <christopherharper123@gmail.com>
2020-10-24 22:27:19 -0600

    Week 6 Update

--- week6/js/ToDo.js
+++ week6/js/ToDo.js
@@ -1,48 +1,151 @@
+// Pulling id's from the HTML and placing them into variables to use.
+const addTodo = document.querySelector("#todoList");
+const addButton = document.querySelector("#addButton");
+const todoList = document.querySelector("#todoLists");
+const completed = document.getElementById("completed");
+const all = document.getElementById("all");
+const uncompleted = document.getElementById("uncompleted");
 
-function saveTodo(todo){
-    const toDoList = getTodoList();
-    toDoList.push(todo);
-    localStorage.setItem('toDoList',JSON.stringify(toDoList));
-}
-//delete id list
-function deleteTodo(id){
-    const toDoList = getTodoList();
-    const updatedTodos=toDoList.filter(todo =>todo.id !=id);
-    localStorage.setItem('toDoList',JSON.stringify(updatedTodos));
-}
-//completed todo
-function completedTodo(todos){    
+
+ // Function to add todo tasks to our html with classes to allow css styling.
+ addButton.addEventListener('click', addToDo);
+ function addToDo() {
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add('todo');
+    const newTodo = document.createElement("div");
+    newTodo.innerText = addTodo.value;
+    saveLocalTasks(addTodo.value);
+    addTodo.value ="";
+    newTodo.classList.add("todoTask");
+    todoDiv.appendChild(newTodo);
     
-    localStorage.setItem('toDoList', JSON.stringify(todos));
-}
+    //Creates button within our html with an event listener of click to activate our toggleCompleted function upon clicking. Then we add the check mark and append it to the html.
+    const completedButton = document.createElement("button");
+    completedButton.addEventListener("click", toggleCompleted);
+    completedButton.innerHTML = "&#9989;";
+    todoDiv.appendChild(completedButton);
+    
+    //Creates button within our html with an event listener of click to activate our deleteCheck function upon clicking. Then we add the X and append it to the html.
+    const trashButton = document.createElement("button");
+    trashButton.addEventListener("click", deleteCheck);
+    trashButton.innerHTML = "&#10060;";
+    todoDiv.appendChild(trashButton);
 
-//get todoList
-function getTodoList(){
-    const todoListString=localStorage.getItem('toDoList');
-    let todoList = [];
-
-    if(todoListString){
-        todoList = JSON.parse(todoListString);
-    } return todoList;
-}
-
-export default {
-    saveTodo,
-    getTodoList,
-    completedTodo,
-    deleteTodo
+    //Sends lists back to the html todoDiv. 
+    todoList.appendChild(todoDiv);
 }
 
 
-
-
-
-
-
-function readFromLS(key) {
-    return localstorage.getItem(key);
+//Function to remove local storage.
+todoList.addEventListener('deleteCheck', deleteCheck);
+function deleteCheck() {
+    const todo = this.parentElement;
+    removeLocalTasks(todo);
+    
+        todo.remove();
 }
 
-function writeToLS(key, data) {
-    localStorage.setItem(key.stringify(data));
+// Function that adds class completed to element. 
+function toggleCompleted() {
+  this.parentElement.classList.toggle('completed');
 }
+
+
+// Event listeners for the filter buttons to activate functions once clicked.
+completed.addEventListener("click", filterTasks);
+all.addEventListener("click", filterTasks);
+uncompleted.addEventListener("click", filterTasks);
+// Function to utilize the filter buttons 
+function filterTasks() {
+    let tasks = this.id; 
+    const todos = todoList.childNodes;
+    todos.forEach(todo => {
+        //Switch statement for all, completed, and uncompleted buttons to change the css style to flex when true and none to hide when false.
+        switch(tasks) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if(todo.classList.contains("completed")){
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains("completed")){
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+        }
+        
+    });
+}
+
+
+// Function that will check if local tasks are stored, if not it'll create an empty array. 
+function saveLocalTasks(todo) {
+  
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    //Adds a new task to the existing local storage array. 
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+
+// Event listener to activate the getTodos function. 
+document.addEventListener("DOMContentLoaded", getTodos);
+// Function that will pull from the local storage / array of all the tasks already in the local storage to display within our html. 
+function getTodos () {
+    let todos;    
+    if(localStorage.getItem('todos') === null){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.forEach(todo => {
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add('todo');
+        const newTodo = document.createElement("div");
+        newTodo.innerText = todo;
+        newTodo.classList.add("todoTask");
+        todoDiv.appendChild(newTodo);
+        
+        //Creates button within our html with an event listener of click to activate our toggleCompleted function upon clicking. Then we add the check mark and append it to the html.
+        const completedButton = document.createElement("button");
+        completedButton.addEventListener("click", toggleCompleted);        
+        completedButton.innerHTML = "&#9989;";
+        todoDiv.appendChild(completedButton);
+        
+        //Creates button within our html with an event listener of click to activate our deleteCheck function upon clicking. Then we add the X and append it to the html.
+        const trashButton = document.createElement("button");
+        trashButton.addEventListener("click", deleteCheck);
+        trashButton.innerHTML = "&#10060;";
+        todoDiv.appendChild(trashButton);
+    
+        //Sends lists back to the html todoDiv. 
+        todoList.appendChild(todoDiv); 
+    });
+}
+
+// Function to remove our local storage data from our array when we are deleting individual lines. 
+function removeLocalTasks(todo) {
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
--- week6/js/ls.js
+++
@@ -1,8 +0,0 @@
-function getTodoList(key){
-    const todoListString=localStorage.getItem('toDoList');
-    let todoList = [];
-
-    if(todoListString){
-        todoList = JSON.parse(todoListString);
-    } return todoList;
-}
